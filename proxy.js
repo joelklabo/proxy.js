88698d2bdf2e7a16c82dfec3d05e2373d96de1dd
Joel Klabo <joelklabo@gmail.com>
2011-11-10 15:18:04 -0800

    slight reformatting

--- proxy.js
+++ proxy.js
@@ -11,8 +11,9 @@ function proxy (host, port, req, res) {
 // Check the headers to see if request is for on of the sites in the
 // sites array. If they match proxy the request
 exports.route = function (req, res, sites) {
+  var host = req.headers.host;
   sites.forEach(function(site){
-    if (req.headers.host == site.host || req.headers.host == 'www.' + site.host) {
+    if (host == site.host || host == 'www.' + site.host) {
       proxy(site.host, site.port, req, res)
       addListeners(proxiedRequest, req, res)
     }
@@ -22,18 +23,10 @@ exports.route = function (req, res, sites) {
 // Add the listeners to the proxied request
 function addListeners (proxiedRequest, req, res) {
   proxiedRequest.addListener('response', function (response) {
-    response.addListener('data', function (chunk) {
-      res.write(chunk, 'binary')
-    })
-    response.addListener('end', function () {
-      res.end()
-    })
+    response.addListener('data', function (chunk) { res.write(chunk, 'binary') })
+    response.addListener('end', function () { res.end() })
     res.writeHead(response.statusCode, response.headers)
   })
-  req.addListener('data', function (chunk) {
-    proxiedRequest.write(chunk, 'binary')
-  })
-  req.addListener('end', function (end) {
-    proxiedRequest.end()
-  })
+  req.addListener('data', function (chunk) { proxiedRequest.write(chunk, 'binary') })
+  req.addListener('end', function (end) { proxiedRequest.end() })
 }  
